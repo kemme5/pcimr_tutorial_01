df5e556b7ab353496adcee5297ea90e0f89e5bfd
kemme5 <kemya1995@gmail.com>
2020-11-24 23:40:59 +0100

    updated sensing front

--- omar_shalaby/src/tutorial2_controller.py
+++ omar_shalaby/src/tutorial2_controller.py
@@ -8,19 +8,20 @@ from geometry_msgs.msg import Quaternion
 from geometry_msgs.msg import Point
 
 
-velval = Twist()
+class bot:
+    
+    def __init__(self):
+        self.command = Twist()
+        self.sub_vel = rospy.Subscriber('/cmd_vel', Twist, self.updateVel)
+        self.sub_scan = rospy.Subscriber('/scan', LaserScan, self.updateScan)
 
-
-def updateVel(data):
-    velval.linear.x= data.linear.x
-
-    velval.angular.z= data.angular.z
-    print(velval)
-
-def updateScan(msg):
+    def updateVel(self,data):
+        self.command.linear.x = data.linear.x
+        self.command.angular.z = data.angular.z
+    def updateScan(self,msg):
     # values at 0 degree
-    if(msg.ranges[122]>0.0):
-        print(msg.ranges[122])
+        if(msg.ranges[122]<3.0 and msg.ranges[122]>-5.0):
+            print("IN")
 
 
 
@@ -29,18 +30,28 @@ def node():
 
     rospy.init_node('tut2_controller', anonymous = True)
     rate = rospy.Rate(1)
+    vel_msg = Twist()
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
 
-    sub_vel = rospy.Subscriber('/cmd_vel', Twist, updateVel)
-    sub_scan = rospy.Subscriber('/scan', LaserScan, updateScan)
+    bot1 = bot()
 
-    pub_move = rospy.Publisher('/pioneer/cmd_vel', Twist, queue_size = 10)
-    pub_move.publish(velval)
-
-    rospy.spin()
+    pub_move = rospy.Publisher('pioneer/cmd_vel', Twist, queue_size = 10)
+    
+    while not rospy.is_shutdown():
+        vel_msg.linear.x = bot1.command.linear.x
+        vel_msg.angular.z = bot1.command.angular.z
+        pub_move.publish(vel_msg)
+        rate.sleep()
+        
 
 if __name__ == '__main__':
-    
-    node()
+    try:
+         node()
+         rospy.spin()
+    except rospy.ROSInterruptException: pass
 
 
 
